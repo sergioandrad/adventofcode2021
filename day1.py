8f83db1bcf195979bb75bbd7369797462958c3db
sergioandrad <39348598+sergioandrad@users.noreply.github.com>
2021-12-08 14:29:41 -0300

    Update day1.py

--- day1.py
+++ day1.py
@@ -15,7 +15,13 @@ os.chdir('E:\\Github\\adventofcode2021')
 
 data = pd.read_csv('input.txt', names = {'depth':1})
 
-pd.merge(data,data.shift(-1), left_index=True, right_index=True, how="inner").assign(
-    flag_inc = lambda dataframe: dataframe['depth_x'] < dataframe['depth_y']).loc[:,'flag_inc'].sum()
+data.assign(depth1 = lambda dataframe: dataframe['depth'].shift(-1)).assign(
+    flag_inc = lambda dataframe: dataframe['depth'] < dataframe['depth1']).loc[:,'flag_inc'].sum()
 
-
+data.assign(depth1 = lambda dataframe: dataframe['depth'].shift(1),
+            depth2 = lambda dataframe: dataframe['depth'].shift(2)
+            ).dropna().assign(
+                sliding3      = lambda dataframe: dataframe['depth']+dataframe['depth1']+dataframe['depth2']).assign(
+                sliding3shift = lambda dataframe: dataframe['sliding3'].shift(-1)).assign(
+                    flag_inc = lambda dataframe: dataframe['sliding3'] < dataframe['sliding3shift']).loc[:,'flag_inc'].sum()
+                    
